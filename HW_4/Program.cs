d6486fa4a6fb1814e1f6cf38eb7d9d12b28a883e
Prokhor <prox-kholopov@yandex.ru>
2022-06-04 01:47:08 +0300

    HW_4

---
+++ HW_4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число: ");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = a % 10;
+
+Console.Write("Последняя цифра вашего числа это: ");
+Console.Write(b);
