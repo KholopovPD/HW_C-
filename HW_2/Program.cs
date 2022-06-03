7b31610c398423b92dd537e91b3c07e8f20eeb2d
Prokhor <prox-kholopov@yandex.ru>
2022-06-03 20:49:49 +0300

    HW_2

---
+++ HW_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое целое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if  (a > max) max = a;
+if  (b > max) max = b;
+if  (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
