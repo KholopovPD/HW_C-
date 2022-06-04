97c39a9d208c60b5263291cd29add191c34f0e3a
Prokhor <prox-kholopov@yandex.ru>
2022-06-04 09:31:04 +0300

    HW_8

---
+++ HW_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+double number = new Random().Next(1, 101);
+
+Console.Write("Случайное число: ");
+
+Console.WriteLine(number);
+
+Console.Write("Введите делитель ");
+
+double a = double.Parse(Console.ReadLine());
+
+double b = number / a;
+
+Console.Write(b);
