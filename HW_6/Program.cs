0f05e8192e031410b82fa7e6552846b089896bf9
Prokhor <prox-kholopov@yandex.ru>
2022-06-04 08:00:48 +0300

    HW_6

---
+++ HW_6/Program.cs
@@ -0,0 +1,28 @@
+﻿int number = new Random().Next(10, 99);
+
+Console.Write("Случайное число: ");
+
+Console.WriteLine(number);
+
+int a = number / 10;
+
+Console.Write("Первая цифра числа: ");
+
+Console.WriteLine(a);
+
+int b = number % 10;
+
+Console.Write("Вторая цифра числа: ");
+
+Console.WriteLine(b);
+
+if (a > b)
+{
+    Console.Write("Наибольшая цифра случайного числа: ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Наибольшая цифра случайного числа: ");
+    Console.WriteLine(b);
+}
